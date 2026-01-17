ff4a35edc5b8e03bd16636c8be9cf35c9cd3a495
Jared Poole <43940867+jarpoole@users.noreply.github.com>
2026-01-17 14:32:23 -0700

    refactor to multiple modules

---
+++ src/camera.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(PanOrbitCameraPlugin)
+            .add_systems(Startup, setup);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    // https://docs.rs/bevy_panorbit_camera/latest/bevy_panorbit_camera/index.html
+    commands.spawn((
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Dir3::Y),
+        PanOrbitCamera {
+            pitch_lower_limit: Some(0.0),
+            allow_upside_down: false,
+            ..default()
+        },
+    ));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,84 @@
-use bevy::prelude::*;
+use avian3d::prelude::*;
+use bevy::{asset::AssetMetaCheck, picking::hover::PickingInteraction, prelude::*};
+use stones::AppPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                canvas: Some("#bevy-canvas".into()),
-                fit_canvas_to_parent: true,
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins((
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        canvas: Some("#bevy-canvas".into()),
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    // https://github.com/bevyengine/bevy/issues/18002
+                    // https://github.com/bevyengine/bevy/pull/10623
+                    meta_check: AssetMetaCheck::Never,
+                    //file_path: "assets".into(),
+                    ..default()
+                }),
+            AppPlugin,
+        ))
         .add_systems(Startup, setup)
         .run();
 }
 
-/// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
-    // circular base
+    // Static physics object with a collision shape
     commands.spawn((
-        Mesh3d(meshes.add(Circle::new(4.0))),
+        RigidBody::Static,
+        Collider::cylinder(4.0, 0.1),
+        Mesh3d(meshes.add(Cylinder::new(4.0, 0.1))),
         MeshMaterial3d(materials.add(Color::WHITE)),
-        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        Restitution::new(0.2),
     ));
-    // cube
+
+    /*
+    commands.spawn(SceneRoot(
+        asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/Fox.glb")),
+        // https://github.com/bevyengine/bevy/discussions/17151
+        //asset_server.load("models/Fox.glb"),
+    ));
+    */
+    //commands.spawn(SceneRoot(asset_server.load("models/Fox.glb#Scene0")));
+
+    // Dynamic physics object with a collision shape and initial angular velocity
+    /*
     commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        RigidBody::Dynamic,
+        //Collider::cuboid(1.0, 1.0, 1.0),
+        ColliderConstructor::TrimeshFromMesh,
+        AngularVelocity(Vec3::new(2.5, 3.5, 1.5)),
+        Mesh3d(meshes.add(Cuboid::from_length(1.0))),
         MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
+        Transform::from_xyz(0.0, 6.0, 0.0),
     ));
-    // light
+    */
+
+    commands.spawn((
+        SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/test.glb"))),
+        RigidBody::Dynamic,
+        ColliderConstructorHierarchy::new(ColliderConstructor::TrimeshFromMesh),
+        AngularVelocity(Vec3::new(2.5, 3.5, 1.5)),
+        Transform::from_xyz(0.0, 6.0, 0.0).with_scale((0.1, 0.1, 0.1).into()),
+        PickingInteraction::default(),
+    ));
+
+    // Light
     commands.spawn((
         PointLight {
             shadows_enabled: true,
             ..default()
         },
         Transform::from_xyz(4.0, 8.0, 4.0),
-    ));
-    // camera
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
